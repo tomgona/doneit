13a4f4114bba9b667ffff9b1aa5d0e2602ee30bf
Gwanez <tgmusha@gmail.com>
2020-08-04 07:48:08 +0300

    changes not to worry about

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
-import React from "react";
-import LogInScreen from "./app/screens/LogInScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
+import React, { useEffect, useState } from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ListingEditScreen />;
 }
