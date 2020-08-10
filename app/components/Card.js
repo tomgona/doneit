592f2f16f5c1de619813a446c32352b8b3d4ced8
Gwanez <tgmusha@gmail.com>
2020-08-10 07:58:57 +0300

    Implementing the onpress prop with touchablewithoutfeedback

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
 
-import AppText from './AppText';
-import colors from '../config/colors';
+import AppText from "./AppText";
+import colors from "../config/colors";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-function Card({title, subTitle, image}) {
-    return (
-        <View style={styles.card}>
-            <Image style={styles.image} source={image}/>
-            <View style={styles.detailsContainer}>
-                <AppText style={styles.title}>{title}</AppText>
-                <AppText style={styles.subtitle}>{subTitle}</AppText>
-            </View>
+function Card({ title, subTitle, image, onPress }) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.card}>
+        <Image style={styles.image} source={image} />
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subtitle}>{subTitle}</AppText>
         </View>
-    );
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: colors.white,
-        borderRadius: 15,
-        marginBottom: 20,
-        overflow: 'hidden',
-    },
-    detailsContainer: {
-        padding: 20,
-    },
-    image: {
-        width: "100%",
-        height: 200,
-    },
-    title: {
-        color: colors.black,
-        marginBottom: 7,
-    },
-    subtitle: { color: colors.secondary,  }
-})
+  card: {
+    backgroundColor: colors.white,
+    borderRadius: 15,
+    marginBottom: 20,
+    overflow: "hidden",
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    color: colors.black,
+    marginBottom: 7,
+  },
+  subtitle: { color: colors.secondary },
+});
 export default Card;
