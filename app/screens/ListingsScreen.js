83a0deb5c5155519cb49437038e606adc07e3667
Gwanez <tgmusha@gmail.com>
2020-08-13 00:03:14 +0300

    using useState useEffect and listingsApi

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
+import listingsApi from "../api/listings";
 
 const listings = [
   {
@@ -21,6 +22,17 @@ const listings = [
   },
 ];
 function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -30,7 +42,7 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"KES" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
