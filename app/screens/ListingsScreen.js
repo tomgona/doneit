f4b1acf4af9f0297c85a2fd249a1cff06c2284ad
Gwanez <tgmusha@gmail.com>
2020-08-14 09:15:39 +0300

    usestate for errors and setting errors and conditionally setting the error var

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -6,23 +6,13 @@ import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 import listingsApi from "../api/listings";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
 
-const listings = [
-  {
-    id: 1,
-    title: "Couch in great condition",
-    price: 10000,
-    image: require("../assets/couch.jpg"),
-  },
-  {
-    id: 2,
-    title: "Selling a red jacket",
-    price: 1200,
-    image: require("../assets/jacket.jpg"),
-  },
-];
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -30,11 +20,20 @@ function ListingsScreen({ navigation }) {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <AppButton title="retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
